77b9d70a3c22bb643ebeee0f63b68036ba19e6ca
OskarLasota <frezzyysmite@gmail.com>
2019-11-12 22:01:13 +0000

    chore: begin working on a lisplist insertionsort algorithm

---
+++ Sorting/src/InsertionSort.java
@@ -0,0 +1,22 @@
+
+public class InsertionSort {
+
+	
+	
+	
+	public static void main(String[] args){
+		
+		
+		
+		
+	}
+	
+	
+	public static LispList<Integer> sort(LispList<Integer> list){
+		
+				
+	}
+	
+	
+	
+}
